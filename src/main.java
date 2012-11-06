6ca3eca24c8824ab206f71d0ec06f3e34cad7c69
Swok <Swok@192.168.1.46>
2012-11-06 15:39:18 +0100

    removed printlns that were for testing purposes

--- src/main.java
+++ src/main.java
@@ -5,8 +5,7 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("something changed for first real commit/push to github \n");
-		System.out.println("something added for second real commit/push to github \n");
+		
 		board playboard = new board();
 		playboard.printBoard();
 		
